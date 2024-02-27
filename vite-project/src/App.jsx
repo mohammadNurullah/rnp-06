b5ece8da480da54ea6027e7949efc8b9c823ab56
mohammadNurullah <nurullahsarkar963@gmail.com>
2024-02-28 00:21:47 +0600

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import TextDisplay from "./TextDisplay";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App</h1>
+      {<TextDisplay text="Learn React" />}
+    </div>
+  );
+};
+
+export default App;
+
+// import TextDisplay from "./TextDisplay";
+
+// const App() {
+//   return (
+//     <div>
+//       <h1>React App</h1>
+//       {<TextDisplay text="Learn React" />}
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ vite-project/src/TextDisplay.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+const TextDisplay = ({ text }) => {
+  return <p>{text}</p>;
+};
+
+export default TextDisplay;
